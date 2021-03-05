d75fa134332883d64483eb4b7c61487447623190
jerrymajewski <jerry.majewski@am.jll.com>
2021-03-05 14:12:59 -0600

    Remove the embedded TableauViz.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import { TableauViz } from './components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Tableau Viz
-        <TableauViz
-          url="https://public.tableau.com/views/COVID-19DataHubGlobalTracker/GLOBAL?:language=en&:embed=y&:origin=viz_share_link&:toolbar=n&:display_count=y&publish=yes"
-        />
+        Tableau Viz Removed
       </header>
     </div>
   );
